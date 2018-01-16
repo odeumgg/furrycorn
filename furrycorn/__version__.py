366a813a19f69a5cf6d9e497a91579ece330618a
Joshua Morris <joshua.a.morris@gmail.com>
2018-01-15 18:56:38 -0800

    setup.py and wheel config

---
+++ furrycorn/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'furrycorn'
+__description__ = 'python jsonapi parser'
+__url__ = 'https://github.com/odeumgg/furrycorn'
+__version__ = '0.7'
+__author__ = 'Joshua Morris'
+__author_email__ = 'joshua.a.morris@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 odeum.gg'
+
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+
+from shutil import rmtree
+from setuptools import find_packages, setup
+
+
+required = ['requests >= 2.18.4', 'simplejson >= 3.13.2', 'urllib3 >= 1.22']
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(os.path.join(here, 'furrycorn', '__version__.py')) as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=long_description,
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(exclude=('tests')),
+    install_requires=required,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
+
